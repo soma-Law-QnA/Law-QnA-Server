958c1f6e5e5f9a2ec5057e082985d27b0c8e67f4
SeungWon <20sunrin041@sunrint.hs.kr>
2021-05-13 02:07:07 +0900

    refactor: router

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,11 @@
 const router = require("express").Router()
+const errors = require("utils/errors")
+
+router.get("/custom-errors", (req, res) => {
+    res.json({
+        errorTypes: errors.errorTypes,
+        errorMessages: errors.errorMessages
+    })
+})
 
 module.exports = router
