a7a895b52451bd989141b335e95fd8a3fb0823ec
hojun <hjsung809@naver.com>
2021-05-13 15:51:31 +0900

    feature/getQnA

--- src/app/qna/controllers.js
+++ src/app/qna/controllers.js
@@ -1,5 +1,41 @@
-const getQnA = async (req, res) => {
-    res.send("test")
+const request = require('request');
+
+const openApiURL = 'http://aiopen.etri.re.kr:8000/LegalQA';
+const access_key = process.env.KEY;
+
+const getQnA = async (req, res) => {  
+    const question = req.query.question;
+
+    if(!question) {
+        return res.status(400).json({
+            result : 1,
+        });
+    }
+
+    const requestJson = {
+        access_key: access_key,
+        argument: {
+            question: question,
+        }
+    };
+
+    const options = {
+        url: openApiURL,
+        body: JSON.stringify(requestJson),
+        headers: {'Content-Type':'application/json; charset=UTF-8'}
+    };
+    
+    request.post(options,
+        function (error, response, body) {
+            if(error) {
+                return res.status(500).json({
+                    result : 1,
+                });
+            }
+
+            res.json(JSON.parse(body));
+        }
+    );
 }
 
 module.exports = getQnA
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-require("dotenv").config()
+// require("dotenv").config()
 const app = require("app")
 app.listen(process.env.PORT || 3000)
 console.log("Server Running at http://localhost:3000")
