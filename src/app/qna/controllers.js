fbb676ef01e6bc744532135a33fedce8b9f4e046
pukuba <20sunrin041@sunrint.hs.kr>
2021-05-13 16:44:20 +0900

    feat: add docker-compose

--- src/app/qna/controllers.js
+++ src/app/qna/controllers.js
@@ -3,12 +3,12 @@ const request = require('request');
 const openApiURL = 'http://aiopen.etri.re.kr:8000/LegalQA';
 const access_key = process.env.KEY;
 
-const getQnA = async (req, res) => {  
+const getQnA = async (req, res) => {
     const question = req.query.question;
 
-    if(!question) {
+    if (!question) {
         return res.status(400).json({
-            result : 1,
+            result: 1,
         });
     }
 
@@ -22,14 +22,14 @@ const getQnA = async (req, res) => {
     const options = {
         url: openApiURL,
         body: JSON.stringify(requestJson),
-        headers: {'Content-Type':'application/json; charset=UTF-8'}
+        headers: { 'Content-Type': 'application/json; charset=UTF-8' }
     };
-    
+
     request.post(options,
         function (error, response, body) {
-            if(error) {
+            if (error) {
                 return res.status(500).json({
-                    result : 1,
+                    result: 1,
                 });
             }
 
